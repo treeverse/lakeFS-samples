eaf8dc6d52ce0b2dd9ae81a5694e274c9eba05b5
johnnyaug <yoni.augarten@treeverse.io>
2023-01-02 15:03:51 +0200

    dag upgrade

---
+++ 06-airflow-dag-example/README.md
@@ -0,0 +1,15 @@
+## lakeFS + Airflow Example
+
+### Run
+
+1. Clone the sample.
+
+1. Spin up the environment:
+   `docker-compose up`
+
+1. Browse to Airflow in [http://localhost:8085/](http://localhost:8085/). User: _airflow_, password: _airflow_.
+
+1. Browse to the lakeFS UI in [http://localhost:8084/](http://localhost:8084/). User: AKIAIOSFODNN7EXAMPLE, password: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY.
+
+1. Run the _etl_ DAG in Airflow and observe the results in lakeFS.
+
--- 06-airflow-dag-example/dags/example.py
+++ 06-airflow-dag-example/dags/example.py
@@ -1,58 +1,109 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from io import BytesIO
 
 import boto3
 import numpy as np
 import pandas as pd
-from airflow import DAG, Dataset
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 from lakefs_provider.operators.commit_operator import LakeFSCommitOperator
+from lakefs_provider.operators.create_branch_operator import LakeFSCreateBranchOperator
+from lakefs_provider.operators.merge_operator import LakeFSMergeOperator
 
-# [START dataset_def]
-dag1_dataset = Dataset("s3://dag1/output_1.txt", extra={"hi": "bye"})
-# [END dataset_def]
-dag2_dataset = Dataset("s3://dag2/output_1.txt", extra={"hi": "bye"})
+RAW_ROW_COUNT = 100000
+
+s3 = boto3.client('s3',
+                  endpoint_url='http://lakefs:8000/',
+                  aws_access_key_id='AKIAIOSFODNN7EXAMPLE',
+                  aws_secret_access_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+                  )
+
+
+def _random_dates():
+    deltas = np.random.randint(0, 60 * 60 * 24 * 365, RAW_ROW_COUNT)
+    base = datetime.now() - timedelta(days=365)
+    return [datetime.fromtimestamp(base.timestamp() + delta).isoformat() for delta in deltas]
+
+
+def _save_data_frame(table_name, dt, df, branch="main"):
+    csv = df.to_csv().encode('utf-8')
+    s3.put_object(
+        Bucket="example-repo",
+        Key=f"{branch}/{table_name}/{dt}/values.csv",
+        Body=BytesIO(csv),
+        ContentLength=len(csv),
+        ContentType='application/csv'
+    )
 
 
 # Extracts raw data and uploads it to lakeFS
 def extract():
     np.random.seed(datetime.now().microsecond)  # TODO - remove this usage of seed
     df = pd.DataFrame({
-        'a': np.random.rand(100000),
-        'b': np.random.rand(100000),
+        'user_id': np.random.randint(1, 1000, RAW_ROW_COUNT),
+        'event_value': np.random.rand(RAW_ROW_COUNT),
+        'event_type': np.random.choice(['click', 'purchase', 'view'], RAW_ROW_COUNT),
+        'event_time': pd.array(_random_dates(), dtype="string"),
     })
-    csv = df.to_csv().encode('utf-8')
-    s3 = boto3.client('s3',
-                      endpoint_url='http://lakefs:8000/',
-                      aws_access_key_id='AKIAIOSFODNN7EXAMPLE',
-                      aws_secret_access_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
-                      )
-    s3.put_object(
-        Bucket="example-repo",
-        Key="main/raw_data/test.csv",
-        Body=BytesIO(csv),
-        ContentLength=len(csv),
-        ContentType='application/csv'
+    dt = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    _save_data_frame("raw_data", dt, df)
+    return dt
+
+
+def _transform_steps(dt_arg, transform_name, transform_func):
+    create_branch_op = LakeFSCreateBranchOperator(
+        task_id=f"{transform_name}_create_branch",
+        repo="example-repo",
+        branch=f"{transform_name}_{dt_arg}",
+        lakefs_conn_id="lakefs",
+        source_branch="main",
     )
 
+    transform_op = PythonOperator(task_id=f"{transform_name}_transform",
+                                          python_callable=transform_func,
+                                          op_kwargs={"dt": dt_arg})
 
-def transform():
-    s3 = boto3.client('s3',
-                      endpoint_url='http://lakefs:8000/',
-                      aws_access_key_id='AKIAIOSFODNN7EXAMPLE',
-                      aws_secret_access_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
-                      )
-    obj = s3.get_object(Bucket="example-repo", Key="main/raw_data/test.csv")
+    commit_op = LakeFSCommitOperator(
+        task_id=f"{transform_name}_commit",
+        repo="example-repo",
+        branch=f"{transform_name}_{dt_arg}",
+        lakefs_conn_id="lakefs",
+        msg="Transform result",
+        metadata={},
+    )
+
+    merge_op = LakeFSMergeOperator(
+        task_id=f"{transform_name}_merge",
+        repo="example-repo",
+        lakefs_conn_id="lakefs",
+        source_ref=f"{transform_name}_{dt_arg}",
+        destination_branch="main",
+        msg="Merge transform result",
+        metadata={},
+    )
+    return (merge_op << commit_op << transform_op << create_branch_op)
+
+
+def transform_total_by_user(dt, **context):
+    obj = s3.get_object(Bucket="example-repo", Key=f"main/raw_data/{dt}/values.csv")
     df = pd.read_csv(obj['Body'])
-    df['c'] = df['a'] + df['b']
-    csv = df.to_csv().encode('utf-8')
-    s3.put_object(
-        Bucket="example-repo",
-        Key="main/transformed/test.csv",
-        Body=BytesIO(csv),
-        ContentLength=len(csv),
-        ContentType='application/csv'
-    )
+    df = df.groupby('user_id').agg({'event_value': 'sum'})
+    _save_data_frame("total_by_user", dt, df, branch=f"total_by_user_{dt}")
+
+
+def transform_total_by_event_type(dt, **context):
+    obj = s3.get_object(Bucket="example-repo", Key=f"main/raw_data/{dt}/values.csv")
+    df = pd.read_csv(obj['Body'])
+    df = df.groupby('event_type').agg({'event_value': 'sum'})
+    _save_data_frame("total_by_event_type", dt, df, branch=f"total_by_event_type_{dt}")
+
+
+def transform_total_by_month(dt, **context):
+    obj = s3.get_object(Bucket="example-repo", Key=f"main/raw_data/{dt}/values.csv")
+    df = pd.read_csv(obj['Body'])
+    df['month'] = pd.to_datetime(df['event_time']).dt.to_period('M')
+    df = df.groupby('month').agg({'event_value': 'sum'})
+    _save_data_frame("transform_total_by_month", dt, df, branch=f"total_by_month_{dt}")
 
 
 with DAG(
@@ -71,13 +122,7 @@ with DAG(
         msg="Extract result",
         metadata={},
     )
-    t3 = PythonOperator(task_id="transform", python_callable=transform)
-    t4 = LakeFSCommitOperator(
-        task_id="commit2",
-        repo="example-repo",
-        branch="main",
-        lakefs_conn_id="lakefs",
-        msg="Transform result",
-        metadata={},
-    )
-    t1 >> t2 >> t3 >> t4
+    t3 = _transform_steps(t1.output, "total_by_user", transform_total_by_user)
+    t4 = _transform_steps(t1.output, "total_by_event_type", transform_total_by_event_type)
+    t5 = _transform_steps(t1.output, "total_by_month", transform_total_by_month)
+    t1 >> t2 >> [t3, t4, t5]
