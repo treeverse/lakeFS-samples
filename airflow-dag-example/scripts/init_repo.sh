09b882a6037a9bc1d67d914d80e8a100891c25fc
rmoff <robin.moffatt@treeverse.io>
2023-06-02 15:05:34 +0100

    Tidyup and fix my dodgy git work

--- airflow-dag-example/scripts/init_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo -e "server:\n    endpoint_url: http://lakefs:8000/api/v1/\ncredentials:\n    access_key_id: AKIAIOSFODNN7EXAMPLE\n    secret_access_key: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" > /home/lakefs/.lakectl.yaml
-
-lakectl branch list lakefs://example-repo || lakectl repo create lakefs://example-repo local://example-repo-${RANDOM}
